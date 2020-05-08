6a50c40669448ef0dc77cf067543a652e8edd180
JANAKIRAM <you@example.com>
2020-05-08 16:25:36 -0400

    homework10

--- robotics_assignments/homework10/src/edge_detect.py
+++ robotics_assignments/homework10/src/edge_detect.py
@@ -13,22 +13,16 @@ class ExtractLines:
         self.bridge = CvBridge()
         #rospy.Subscriber("white_line", Image, self.Lines_in_bw_cb)
         #rospy.Subscriber("yellow_line", Image, self.Lines_in_bw_cb)
-        rospy.Subscriber("cropped", Image, self.Combine_and_publish)
+        #rospy.Subscriber("cropped", Image, self.Combine_and_publish)
         rospy.Subscriber("white_line", Image, self.Lines_white)
         rospy.Subscriber("yellow_line", Image, self.Lines_yellow)
-        self.pubw = rospy.Publisher("Line_white", Image, queue_size=10)
-        self.puby = rospy.Publisher("Line_yellow", Image, queue_size=10)
-        
-        self.img1 = None
-        self.img2 = None
-        self.img3 = None
-        self.cb1 = 0 
-        self.cb2 = 0
+        self.pubw = rospy.Publisher("hough_white", Image, queue_size=10)
+        self.puby = rospy.Publisher("hough_yellow", Image, queue_size=10)
    
     def Combine_and_publish(self,msg):
         
-        self.img3 = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-        return self.img3
+        cv_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+
 
     def output_lines(self, original_image, lines):
 
@@ -47,19 +41,19 @@ class ExtractLines:
                 cv2.circle(output, (l[2],l[3]), 2, (0,0,255))
         return output
 
-
     def Lines_yellow(self,msg):
 
         cv_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
 
         hsv_yellow = cv2.cvtColor(cv_img, cv2.COLOR_BGR2HSV)
         
-        edges = cv2.Canny(hsv_yellow, 0, 300, apertureSize=3)
+        edges = cv2.Canny(hsv_yellow, 0, 500, apertureSize=3)
 
-        self.img1 = self.output_lines(cv_img, edges)
-      
-        self.cb1 = 1
+        yellow_output = self.output_lines(hsv_yellow, edges)
 
+        cv_image = self.bridge.cv2_to_imgmsg(yellow_output, "bgr8")
+        
+        self.puby.publish(cv_image)
 
     def Lines_white(self,msg):
 
@@ -69,24 +63,16 @@ class ExtractLines:
        
         edges = cv2.Canny(hsv_white, 0, 300, apertureSize=3)
 
-        self.img2 = self.output_lines(cv_img, edges)
+        white_output = self.output_lines(cv_img, edges)
 
-        self.cb2 = 1
+        cv_image = self.bridge.cv2_to_imgmsg(white_output, "bgr8")
         
+        self.pubw.publish(cv_image)
+
+
       
 if __name__=="__main__":
     # initialize our node and create a publisher as normal
     rospy.init_node("edge_detect", anonymous=True)
     img_flip = ExtractLines()
-    
-    while img_flip.cb1 == 1 and img_flip.cb2 == 1: 
-
-        img = cv2.bitwise_and(img_flip.img1, img_flip.img2, mask=None)
-        image_final = img_flip.bridge.cv2_to_imgmsg(img, "bgr8")
-        img_flip.puby.publish(image_final)
-        self.cb1 = 0
-        self.cb2 = 0
-        break
-
-        #self.pubw.publish(img_flip.cv_image2)
     rospy.spin()
